acf54472b82e915cc8a139c45e4491f627f20cd7
Melroy van den Berg <melroy@melroy.org>
2026-01-12 22:08:23 +0100

    Since difficulty algo is not correct, lets show halving by default for now

--- frontend/src/app/components/difficulty/difficulty.component.ts
+++ frontend/src/app/components/difficulty/difficulty.component.ts
@@ -63,7 +63,7 @@ export class DifficultyComponent implements OnInit {
   isLoadingWebSocket$: Observable<boolean>;
   difficultyEpoch$: Observable<EpochProgress>;
 
-  mode: 'difficulty' | 'halving' = 'difficulty';
+  mode: 'difficulty' | 'halving' = 'halving';
   userSelectedMode: boolean = false;
 
   now: number = Date.now();
