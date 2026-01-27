07f3dc92cbe54f7784a48d729d9b67341eed34d3
Melroy van den Berg <melroy@melroy.org>
2026-01-28 00:03:36 +0100

    Next to a cache, also created a failed list

--- frontend/src/app/components/transactions-list/transactions-list.component.ts
+++ frontend/src/app/components/transactions-list/transactions-list.component.ts
@@ -609,9 +609,9 @@ export class TransactionsListComponent implements OnInit, OnChanges, OnDestroy {
         category, // For later reference
         metadata$: this.bcmrService.getBcmrMetadata(category).pipe(
           catchError((error) => {
-            console.info(
-              `Failed to fetch BCMR metadata for category ${category}: ${error.message}.`
-            );
+            // console.info(
+            //   `Failed to fetch BCMR metadata for category ${category}: ${error.message}.`
+            // );
             // Return null or a default value when the request fails
             return of(null as BcmrMetadata | null);
           })
