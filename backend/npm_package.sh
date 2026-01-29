81d11b9be7b51c209791a5aa9dbf7f90e66b5d04
Melroy van den Berg <melroy@melroy.org>
2026-01-29 23:53:04 +0100

    Add health check

--- backend/npm_package.sh
+++ backend/npm_package.sh
@@ -1,15 +1,17 @@
 #/bin/sh
 set -e
 
-# Remove previous dist folder
+# Remove previous dist folder (if present)
 rm -rf dist
 # Build new dist folder
 npm run build
-# Remove previous package folder
+# Remove previous package folder (if present)
 rm -rf package
 # Move JS and deps
 mv dist package
 cp -R node_modules package
+# Copy healthcheck.js
+cp healthcheck.js package
 # Remove symlink for rust-gbt and insert real folder
 rm package/node_modules/rust-gbt
 cp -R rust-gbt package/node_modules
