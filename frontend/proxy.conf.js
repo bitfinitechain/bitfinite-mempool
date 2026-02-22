cad988c5b6359bae7d4aba24fc77242122c1128d
Melroy van den Berg <melroy@melroy.org>
2026-02-22 04:27:31 +0100

    Fix proxy.conf.js

--- frontend/proxy.conf.js
+++ frontend/proxy.conf.js
@@ -1,20 +1,11 @@
 const PROXY_CONFIG = [
   {
     context: [
-      "*",
       "/api/**",
-      "!/api/v1/ws",
       "/testnet/api/**",
       "/signet/api/**",
       "/testnet4/api/**",
     ],
-    target: "https://bchexplorer.cash",
-    ws: true,
-    secure: false,
-    changeOrigin: true,
-  },
-  {
-    context: ["/api/v1/ws"],
     target: "https://bchexplorer.cash",
     ws: true,
     secure: false,
@@ -27,11 +18,14 @@ const PROXY_CONFIG = [
     changeOrigin: true,
   },
   {
-    context: [
-      "/resources/assets.json",
-      "/resources/assets.minimal.json",
-      "/resources/worldmap.json",
-    ],
+    context: ["/api/v1/ws"],
+    target: "https://bchexplorer.cash",
+    ws: true,
+    secure: false,
+    changeOrigin: true,
+  },
+  {
+    context: ["/resources/worldmap.json"],
     target: "https://bchexplorer.cash",
     secure: false,
     changeOrigin: true,
