594c0a6f85e80383ac1ad6d8d3ac23b6d717af59
Melroy van den Berg <melroy@melroy.org>
2026-02-25 16:02:44 +0100

    Show all the hex to utf8 (ascii), instead of just the first hex values

--- frontend/src/app/shared/pipes/hex2ascii/hex2ascii.pipe.ts
+++ frontend/src/app/shared/pipes/hex2ascii/hex2ascii.pipe.ts
@@ -6,17 +6,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class Hex2asciiPipe implements PipeTransform {
   transform(hex: string): string {
-    const opPush = hex
-      .split(' ')
-      .filter((_, i, a) => i > 0 && /^OP_PUSH/.test(a[i - 1]));
-
-    if (opPush[0]) {
-      hex = opPush[0];
-    }
-
     if (!hex) {
       return '';
     }
+    const tokens = hex.split(' ');
+    const opPush = tokens.filter(
+      (_, i, a) => i > 0 && /^OP_PUSH/.test(a[i - 1])
+    );
+
+    if (opPush.length > 0) {
+      return opPush.map((h) => this.hexToAscii(h)).join(' ');
+    }
+
+    return this.hexToAscii(hex);
+  }
+
+  private hexToAscii(hex: string): string {
     const bytes: number[] = [];
     for (let i = 0; i < hex.length; i += 2) {
       bytes.push(parseInt(hex.substr(i, 2), 16));
