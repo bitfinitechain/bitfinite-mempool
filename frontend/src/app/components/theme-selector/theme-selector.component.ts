a3742a772fd16b1fac337d86e787c75585f70bc4
Melroy van den Berg <melroy@melroy.org>
2026-05-08 22:11:07 +0200

    Call it dark and light mode

--- frontend/src/app/components/theme-selector/theme-selector.component.ts
+++ frontend/src/app/components/theme-selector/theme-selector.component.ts
@@ -12,7 +12,7 @@ import { Subscription } from 'rxjs';
 })
 export class ThemeSelectorComponent implements OnInit {
   themeForm: UntypedFormGroup;
-  themes = ['default', 'bch', 'contrast', 'softsimon', 'bukele', 'light'];
+  themes = ['default', 'light', 'contrast', 'softsimon', 'bukele'];
   themeSubscription: Subscription;
 
   constructor(
